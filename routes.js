02326f4d8ab51efccb92e766a34f9be7c2fc8675
Kim Fitter <kimfitter@yahoo.com>
2020-06-11 23:23:59 +1200

    Get root route working

--- routes.js
+++ routes.js
@@ -1,1 +1,10 @@
+const express = require('express')
+const router = express.Router()
 
+const db = require('./db')
+
+router.get('/', (req, res) => {
+  res.send('Subway !')
+})
+
+module.exports =router
--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+const express = require('express')
+const hbs = require('express-handlebars')
 
+const routes = require('./routes')
+
+const server = express()
+
+server.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultLayout: 'main'
+}))
+server.set('view engine', 'hbs')
+server.use(express.static('public'))
+server.use(express.urlencoded({ extended: true }))
+
+server.use('/', routes)
+
+module.exports = server

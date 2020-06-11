816cd040342ec73f0741509376319ef13885441b
Kim Fitter <kimfitter@yahoo.com>
2020-06-12 11:06:36 +1200

    Update root to users object to be pased to index template

--- routes.js
+++ routes.js
@@ -5,34 +5,34 @@ const db = require('./db')
 
 router.get('/', (req, res) => {
     db.getUsers()
-    .then(users => {
-      res.render('index', { users: username })
-    })
-    .catch(err => {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
-    })
+        .then(users => {
+            res.render('index', { users: users })
+        })
+        .catch(err => {
+            res.status(500).send('DATABASE ERROR: ' + err.message)
+        })
 })
 
 router.get('/select/:id', (req, res) => {
     const id = Number(req.params.id)
     db.getSelections(id) //tbd
-    .then(selections => { //tbd
-      res.render('picker', { }) //tbd
-    })
-    .catch(err => {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
-    })
+        .then(selections => { //tbd
+            res.render('picker', {}) //tbd
+        })
+        .catch(err => {
+            res.status(500).send('DATABASE ERROR: ' + err.message)
+        })
 })
 
 router.get('/sandwich/:id', (req, res) => {
     const id = Number(req.params.id)
     db.getFave(id)
-    .then(user => {
-      res.render('display', { sub: sub })
-    })
-    .catch(err => {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
-    })
+        .then(user => {
+            res.render('display', { sub: sub })
+        })
+        .catch(err => {
+            res.status(500).send('DATABASE ERROR: ' + err.message)
+        })
 })
 
 module.exports = router

edd5e0db63a68f233eea867b20e168ba13563bef
Jake Hurley <63322006+jake-hurley@users.noreply.github.com>
2020-06-12 13:31:02 +1200

    create main hbs layout

--- routes.js
+++ routes.js
@@ -4,35 +4,37 @@ const router = express.Router()
 const db = require('./db')
 
 router.get('/', (req, res) => {
-    db.getUsers()
-        .then(users => {
-            res.render('index', { users: users })
-        })
-        .catch(err => {
-            res.status(500).send('DATABASE ERROR: ' + err.message)
-        })
+  db.getUsers()
+    .then(users => {
+      res.render('index', { users: users })
+    })
+    .catch(err => {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
 })
 
 router.get('/select/:id', (req, res) => {
-    const id = Number(req.params.id)
-    db.getSelections(id) //tbd
-        .then(selections => { //tbd
-            res.render('picker', {}) //tbd
-        })
-        .catch(err => {
-            res.status(500).send('DATABASE ERROR: ' + err.message)
-        })
+  const id = Number(req.params.id)
+  db.getSelections(id) // tbd
+    .then(selections => { // tbd
+      res.render('picker', {}) // tbd
+    })
+    .catch(err => {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
 })
 
-router.get('/sandwich/:id', (req, res) => {
-    const id = Number(req.params.id)
-    db.getFave(id)
-        .then(user => {
-            res.render('display', { sub: sub })
-        })
-        .catch(err => {
-            res.status(500).send('DATABASE ERROR: ' + err.message)
-        })
+router.post('/sandwich/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const body = req.params.body
+  console.log(body)
+  db.getFave(id)
+    .then(user => {
+      res.render('display', { sub: sub })
+    })
+    .catch(err => {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const routes = require('./routes')
 const server = express()
 
 server.engine('hbs', hbs({
-    extname: 'hbs',
-    defaultLayout: 'main'
+  extname: 'hbs',
+  defaultLayout: 'main'
 }))
 server.set('view engine', 'hbs')
 server.use(express.static('public'))
